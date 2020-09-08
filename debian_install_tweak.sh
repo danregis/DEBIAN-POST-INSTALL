b4c5bd6fdac605226a79ce6770eeb8ba05eb11a0
danregis <danregis@users.noreply.github.com>
2020-09-08 10:20:30 -0400

    Update debian_install_tweak.sh

--- debian_install_tweak.sh
+++ debian_install_tweak.sh
@@ -8,14 +8,21 @@
 # |-------------------------------------|
 # | OS             | Test | Last test   |
 # |----------------|------|-------------|
-# | Debian 10.3    | OK   | 10 Mar 2020 |
+# | Debian 10.3    |  OK  | 10 Mar 2020 |
+# |                |      |             |
+# |                |      |             |
+# |                |      |             |
+# |----------------|------|-------------|
 
+###############################################################
 print_help {
          echo "Please make sure of your selections -- they are unreversible"
          echo "This will update, install and tweak your debian install"
          }
 
-########Modifying Repo ###################################
+########Modifying Repo ########################################
+
+#need to determine which distro is used (for now debian is by default)
 
 repo_mod () {
      #sed '/deb/s/$/ non-free/' /etc/apt/sources.list
@@ -35,9 +42,7 @@ echo deb http://deb.debian.org/debian buster-backports main > /etc/apt/sources.l
 ################################################################
 echo "updating, upgrading, Please waite..."
 
-function_name () {
-<commands>
-}
+main_update () {
 
 sudo apt -y clean                                # REMOVE UPDATE DB
 sudo apt -y autoclean                            # REMOVE NOT UNUSED PACKAGES
@@ -46,15 +51,21 @@ sudo apt -y update                               # UPDATE LATEST PKG
 sudo apt -y upgrade                              # UPGRADE PKG
 sudo apt -y dist-upgrade                         # UPGRADE DISTRIBUTION
 sudo logrotate -vf /etc/logrotate.conf           # Force Rotate logs
+}
+
 
 ################################################################
 
 # 1.5 INSTALL FIRMWARE AND MICROCODE
 
+
+firmware_update () {
+
 sudo apt -y install firmware-misc-nonfree
 sudo apt -y install intel-microcode
 sudo apt -y install iucode-tool
 sudo apt -y install ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
+}
 
 ################################################################
 
@@ -75,28 +86,37 @@ sudo apt -y install default-jdk                                        # JAVA DE
 sudo apt -y install wavemon                                            # NET ONLY FOR Wireless
 sudo apt -y install speetest-CLI                                       # Speed test tool
 
-
 ###############################################################
 
 # 3. GUI SOFTWARE
+
+soft_install () {
 
 sudo apt -y install gparted                                                 # PARTITION TOOL
 sudo apt -y install gvfs-backends ntfs-3g                                   # USERSPACE VIRTUAL FILESYSTEM
 sudo apt -y install xarchiver                                               # FILE ARCHIVER FRONTEND
 sudo apt -y install galculator                                              # SCIENTIFIC CALCULATOR
 sudo apt -y install vlc                                                     # VIDEO AND AUDIO PLAYER
+sudo apt -y install mpv                                                     # VIDEO AND AUDIO PLAYER
 sudo apt -y install blender imagemagick inkscape                            # GRAPHICS EDITORS
 sudo apt -y install gimp gimp-data gimp-plugin-registry gimp-data-extras -y # GIMP WTH EXTRAS
 sudo apt -y install audacity                                                # AUDIO EDITOR
 sudo apt -y install openshot                                                # VIDEO EDITOR
 sudo apt -y install filezilla                                               # FTP/FTPS/SFTP CLIENT
 sudo apt -y install libreoffice                                             # OFFICE (optional, not last version)
-sudo apt -y install google-chrome-stable
+#sudo apt -y install google-chrome-stable
 sudo apt -y install kazam                                                   # SCREENCAST (optional)
+sudo apt -y lshw                                                            # Information about hardware configuration 
+}
 
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -p /opt/visualtmp/      ## vscode studio
-dpkg -i /opt/visualtmp/*.deb
-apt -y get install -f
+###############################################################
+
+# 3.2 Install some .deb packages
+
+wget https://github.com/VSCodium/vscodium/releases/download/1.48.2/codium_1.48.2-1598439200_amd64.deb -O /opt/  #visual studio code foos version
+cd /opt/
+dpkg -i codium*.deb
+apt-get install -f 
 
 ###############################################################
 
@@ -112,18 +132,23 @@ lshw -numeric -C display | grep vendor
 
 # 4. SERVICES
 
-sudo apt -y install ufw                                                      # firewall
-sudo systemctl start ufw
-sudo systemctl enable ufw
+read -p "would you like to activate firewall (y/n)?" choice
+case "$choice" in 
+  y|Y ) sudo apt -y install ufw; systemctl start ufw; systemctl enable ufw;; # Firewall
+  n|N ) echo "Ok, continuing...";;
+  * ) echo "invalid";;
+esac
 
+###############################################################
 
 #ask if a laptop
 
-echo 'Is this a laptop ?'
-sudo apt -y install tlp                                                      # battery saver
-sudo systemctl start tlp
-sudo systemctl enable tlp
-
+read -p "Is this a laptop (y/n)?" choice
+case "$choice" in 
+  y|Y ) sudo apt -y install tlp; systemctl start tlp; systemctl enable tlp;; # batt saver
+  n|N ) echo "Ok, continuing....";;
+  * ) echo "invalid";;
+esac
 
 ##############################################################
 
@@ -131,6 +156,8 @@ sudo systemctl enable tlp
 
 sudo sysctl vm.swappiness=10                                                 # SET SWAPINESS
 sudo hdparm -W 1 /dev/sda                                                    # SET DISK CACHE ON
+
+#remove time stamp on fstab
 
 #set grub
 
