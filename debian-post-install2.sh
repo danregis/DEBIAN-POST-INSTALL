4d6e57e54957806b5c1f2c2f50c84f1462c078fb
danregis <dregis@gmail.com>
2023-04-01 09:07:54 -0400

    chat4-B

--- debian-post-install2.sh
+++ debian-post-install2.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
@@ -6,84 +8,88 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 add_repo() {
-    apt update
+    sudo apt update
     if ! command -v lsb_release &> /dev/null; then
         echo "lsb-release not found. Installing it now..."
-        apt install -y lsb-release
+        sudo apt install -y lsb-release
     fi
 
     release_codename=$(lsb_release -sc)
 
-    cat <<- EOF > /etc/apt/sources.list
-        deb http://deb.debian.org/debian/ $release_codename main contrib non-free
-        deb-src http://deb.debian.org/debian/ $release_codename main
-        deb http://security.debian.org/debian-security $release_codename/updates main contrib non-free
-        deb-src http://security.debian.org/debian-security $release_codename/updates main
-        deb http://deb.debian.org/debian/ $release_codename-updates main contrib non-free
-        deb-src http://deb.debian.org/debian/ $release_codename-updates main
-        deb http://deb.debian.org/debian $release_codename-backports main
+    sudo tee /etc/apt/sources.list > /dev/null << EOF
+deb http://deb.debian.org/debian/ $release_codename main contrib non-free
+deb-src http://deb.debian.org/debian/ $release_codename main
+deb http://security.debian.org/debian-security $release_codename/updates main contrib non-free
+deb-src http://security.debian.org/debian-security $release_codename/updates main
+deb http://deb.debian.org/debian/ $release_codename-updates main contrib non-free
+deb-src http://deb.debian.org/debian/ $release_codename-updates main
+deb http://deb.debian.org/debian $release_codename-backports main
 EOF
 }
 
 updates() {
-    apt -y clean
-    apt -y autoclean
-    apt -y autoremove
-    apt update
-    apt -y upgrade
-    apt -y dist-upgrade
-    logrotate -vf /etc/logrotate.conf
-    rm -f /bin/update
+    sudo apt -y clean
+    sudo apt -y autoclean
+    sudo apt -y autoremove
+    sudo apt update
+    sudo apt -y upgrade
+    sudo apt -y dist-upgrade
+    sudo logrotate -vf /etc/logrotate.conf
+    sudo rm -f /bin/update
 }
 
 firmware() {
-    apt -y install firmware-misc-nonfree intel-microcode iucode-tool \
+    sudo apt -y install firmware-misc-nonfree intel-microcode iucode-tool \
     "ttf-mscorefonts-installer" rar unrar libavcodec-extra gstreamer1.0-libav \
     gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
 }
 
 cli_install() {
-    apt -y install build-essential cmake p7zip p7zip-full unrar-free unzip \
+    sudo apt -y install build-essential cmake p7zip p7zip-full unrar-free unzip \
     htop lshw wget locate curl net-tools rsync tmux nano git ffmpeg \
     default-jdk wavemon speedtest-cli
 }
 
 gui_install() {
-    apt -y install gparted galculator vlc mpv blender imagemagick inkscape gimp \
+    sudo apt -y install gparted galculator vlc mpv blender imagemagick inkscape gimp \
     gimp-data gimp-plugin-registry gimp-data-extras audacity openshot filezilla \
     libreoffice firefox
 }
 
 deb_install() {
-    apt install -y software-properties-common apt-transport-https curl
+    sudo apt install -y software-properties-common apt-transport-https curl
     curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-    apt update
-    apt install -y "code"
+    sudo apt update
+    sudo apt install -y "code"
 }
 
 gpu_drivers() {
     video=$(lshw -numeric -C display | grep vendor)
 
-    if [[ "$video" == *"nvidia"* ]]; then
-        add-apt-repository ppa:graphics-drivers/ppa -y
-        apt update
-        apt install -y nvidia-driver
+    if [[ "$video" == *\"nvidia"* ]]; then
+        sudo add-apt-repository ppa:graphics-drivers/ppa -y
+        sudo apt update
+        sudo apt install -y nvidia-driver
     elif [[ "$video" == *"amd"* ]]; then
-        add-apt-repository ppa:oibaf/graphics-drivers -y
-        apt update
-        apt install -y amdgpu-pro
+        sudo add-apt-repository ppa:oibaf/graphics-drivers -y
+        sudo apt update
+        sudo apt install -y amdgpu-pro
     fi
 }
 
 firewall() {
     if [[ $(systemctl is-active ufw) != "active" ]]; then
-        apt -y install ufw
-        systemctl start ufw
+        sudo apt -y install ufw
+        sudo systemctl start ufw
     fi
     if [[ $(systemctl is-enabled ufw) != "enabled" ]]; then
-        systemctl enable ufw
+        sudo systemctl enable ufw
     fi
+}
+
+tweaks() {
+    echo "Tweaks applied: none"
 }
 
 laptop() {
@@ -93,17 +99,13 @@ laptop() {
     fi
     
     if [[ $(systemctl is-active tlp) != "active" ]]; then
-        apt -y install tlp
-        systemctl start tlp
+        sudo apt -y install tlp
+        sudo systemctl start tlp
     fi
     if [[ $(systemctl is-enabled tlp) != "enabled" ]]; then
-        systemctl enable tlp
+        sudo systemctl enable tlp
     fi
-}
-
-tweaks() {
-    echo "# vm.swappiness=10" >> /etc/sysctl.conf
-    echo "Tweaks applied: none" # more context should be given
+    tweaks
 }
 
 PS3='Make your selection: '
@@ -137,7 +139,6 @@ do
             ;;
         "Laptop_Tweaks")
             laptop
-            tweaks
             ;;
         "Quit")
             break
